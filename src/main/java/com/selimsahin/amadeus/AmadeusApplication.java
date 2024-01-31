adbac674b7ebd3a9945799648ae2ef398963c8de
Selim Şahin <selimsahin.sns@gmail.com>
2024-02-01 00:53:10 +0300

    Initial commit

---
+++ src/main/java/com/selimsahin/amadeus/AmadeusApplication.java
@@ -0,0 +1,13 @@
+package com.selimsahin.amadeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmadeusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmadeusApplication.class, args);
+	}
+
+}
